0bbe37593b30f62653efb74af03c627769217fa0
asoikaw15 <krteammain@gmail.com>
2022-05-15 11:57:13 +0900

    Revert "Revert "hello""
    This reverts commit eaf3500a94c5fb98ba79f41322eb95b8939c4ca9.

---
+++ routes/users/users.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const Accounts = require('../models/account');
+const passport = require('passport')
+const catchasync = require('../../utils/catchasync');
+const {isLoggedIn} = require('../../authmiddleware');
+const { redirect } = require('express/lib/response');
+
+
+
+
+
+
+router.get('/register', (req,res)=>{
+    res.render('register');
+})
+router.post('/register',catchasync (async (req,res)=>{
+try{
+   const {name, username, email, password} = req.body.newu;
+   const user = new Accounts({email, name, username});
+   const regUser = await Accounts.register(user, password);
+   console.log(regUser);
+   req.flash('success', "Thank you for registering " + name ,", you may now log in.");
+   req.session.destroy();
+   res.redirect('login');
+}
+catch (e) {
+    req.flash("error", e.message);
+    res.redirect('register');
+}
+}))
+
+router.get('/login', (req,res)=>{
+    res.render('login');
+    
+})
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/profile';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+})
+
+router.get('/logout', (req,res)=>{
+    req.logout();
+    req.flash('success', 'Thank You.');
+    
+    res.redirect('/');
+    
+})
+module.exports = router;
